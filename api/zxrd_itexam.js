26c80980fcdc8ba843e583a1f7a3a8751db8ee2b
CodeZhangBorui <61909157+CodeZhangBorui@users.noreply.github.com>
2022-05-15 10:01:00 +0800

    zxrd-itexams update

--- api/zxrd_itexam.js
+++ api/zxrd_itexam.js
@@ -1,18 +1,20 @@
 function EXAM() {
     ActiveID = prompt("请输入授权码","");
     ExamID = prompt("请输入全真考场序号","");
-    Score = prompt("请输入成绩（一定要乘100！）","12000");
+    ScoreIpt = Number(prompt("请输入成绩",120));
+    Score = String(ScoreIpt*100)
     JumpURL = "http://www.itexam.com/User/submitscore/" + ActiveID + "_EXAM" + ExamID + "_" + Score;
     console.log("JumpURL = " + JumpURL);
-    alert("即将跳转：" + JumpURL);
+    alert(`授权码：${ActiveID}\n考场序号：${ExamID}\n成绩：${ScoreIpt}\n即将跳转：${JumpURL}`);
     window.location.href = JumpURL;
 }
 function Moni() {
     ActiveID = prompt("请输入授权码","");
     ExamID = prompt("请输入全真考场序号","");
-    Score = prompt("请输入成绩（一定要乘100！）","12000");
+    ScoreIpt = Number(prompt("请输入成绩",120));
+    Score = String(ScoreIpt*100)
     JumpURL = "http://www.itexam.com/User/submitscore/" + ActiveID + "_Moni" + ExamID + "_" + Score;
     console.log("JumpURL = " + JumpURL);
-    alert("即将跳转：" + JumpURL);
+    alert(`授权码：${ActiveID}\n考场序号：${ExamID}\n成绩：${ScoreIpt}\n即将跳转：${JumpURL}`);
     window.location.href = JumpURL;
 }
