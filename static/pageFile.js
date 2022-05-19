63f585205981d6a63a8231a4f108ad07db3dc4dc
CodeZhangBorui <61909157+CodeZhangBorui@users.noreply.github.com>
2022-05-19 18:11:12 +0800

    autofill test

---
+++ static/pageFile.js
@@ -0,0 +1,15 @@
+function fill_menuBar() {
+  fillContent = '\
+<li class="layui-nav-item layui-this"><a href="/">主页</a></li>\
+<li class="layui-nav-item"><a href="/article/index">文章</a></li>\
+<li class="layui-nav-item">\
+  <a href="javascript:;">工作室</a>\
+  <dl class="layui-nav-child"> <!-- 二级菜单 -->\
+    <dd><a href="/studio/powercode">PowerCode Studio</a></dd>\
+  </dl>\
+</li>\
+<li class="layui-nav-item"><a href="/codespace.html">CodeSpace</a></li>\
+<li class="layui-nav-item"><a href="/downloads">资源下载</a></li>\
+'
+  document.getElementById("menuBar").innerHTML = fillContent;
+}
