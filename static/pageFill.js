a466cf860c65d4830eba68e499eba6cf485efb59
CodeZhangBorui <61909157+CodeZhangBorui@users.noreply.github.com>
2022-05-20 08:35:30 +0800

    pagefill this-element add

--- static/pageFill.js
+++ static/pageFill.js
@@ -1,15 +1,16 @@
-function fill_menuBar() {
+function fill_menuBar(this_element) {
   fillContent = '\
-<li class="layui-nav-item layui-this"><a href="/">主页</a></li>\
-<li class="layui-nav-item"><a href="/article/index">文章</a></li>\
-<li class="layui-nav-item">\
+<li id="menuBar_Home" class="layui-nav-item layui-this"><a href="/">主页</a></li>\
+<li id="menuNar_Article" class="layui-nav-item"><a href="/article/index">文章</a></li>\
+<li id="menuBar_Studio" class="layui-nav-item">\
   <a href="javascript:;">工作室</a>\
   <dl class="layui-nav-child"> <!-- 二级菜单 -->\
     <dd><a href="/studio/powercode">PowerCode Studio</a></dd>\
   </dl>\
 </li>\
-<li class="layui-nav-item"><a href="/codespace.html">CodeSpace</a></li>\
-<li class="layui-nav-item"><a href="/downloads">资源下载</a></li>\
+<li id="menuBar_Codespace" class="layui-nav-item"><a href="/codespace.html">CodeSpace</a></li>\
+<li id="menuBar_Downloads" class="layui-nav-item"><a href="/downloads">资源下载</a></li>\
 '
   document.getElementById("menuBar").innerHTML = fillContent;
+  document.getElementById("menuBar_" + this_element).className += " layui-this";
 }
