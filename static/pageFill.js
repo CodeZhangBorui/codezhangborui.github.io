bbf32bfb7935a84ffcae1b6da6aee8a82b3acf7c
CodeZhangBorui <61909157+CodeZhangBorui@users.noreply.github.com>
2022-05-20 09:20:37 +0800

    bug?

--- static/pageFill.js
+++ static/pageFill.js
@@ -1,6 +1,6 @@
 function fill_menuBar(this_element) {
   fillContent = '\
-<li id="menuBar_Home" class="layui-nav-item layui-this"><a href="/">主页</a></li>\
+<li id="menuBar_Home" class="layui-nav-item"><a href="/">主页</a></li>\
 <li id="menuNar_Article" class="layui-nav-item"><a href="/article/index">文章</a></li>\
 <li id="menuBar_Studio" class="layui-nav-item">\
   <a href="javascript:;">工作室</a>\
