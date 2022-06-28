2ff3214114e252692c77d954abf63962675748be
CodeZhangBorui <61909157+CodeZhangBorui@users.noreply.github.com>
2022-06-28 18:41:44 +0800

    new pageFill

--- static/pageFill.js
+++ static/pageFill.js
@@ -16,3 +16,29 @@ function fill_menuBar(this_element) {
   document.getElementById("menuBar").innerHTML = fillContent;
   document.getElementById("menuBar_" + this_element).className += " layui-this";
 }
+
+function fill_footBlock() {
+  fillContent = `
+<div class="acrylic">
+  <p>Copyright(c) CodeZhangBorui & PowerCode 2022, All Right Reserved.</p>
+  <p>由 Github Page 提供支持</p>
+  <p>联系站长：[Discord] Code张博睿#4080</p>
+  <p>
+    主页:
+    <a href="https://codezhangborui.github.io/" target="_blank" style="color: #f3f3f3;">
+      https://codezhangborui.github.io/
+    </a>
+  </p>
+  <p>
+    Github 仓库:
+    <a href="https://github.com/CodeZhangBorui/codezhangborui.github.io/" target="_blank" style="color: #f3f3f3;">
+      https://github.com/CodeZhangBorui/codezhangborui.github.io/
+    </a>
+  </p>
+  <iframe
+    src="https://ghbtns.com/github-btn.html?user=codezhangborui&repo=codezhangborui.github.io&type=star&count=true"
+    frameborder="0" scrolling="0" width="150" height="20" title="GitHub Star"></iframe>
+</div>
+`
+  document.getElementById("footblock").innerHTML = fillContent;
+}
