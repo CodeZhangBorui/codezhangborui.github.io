cd4a23009cee1cb86b28dfc049236cbaa41dd7a3
CodeZhangBorui <61909157+CodeZhangBorui@users.noreply.github.com>
2022-06-29 07:26:26 +0800

    change pagefill

--- static/pageFill.js
+++ static/pageFill.js
@@ -19,25 +19,19 @@ function fill_menuBar(this_element) {
 
 function fill_footBlock() {
   fillContent = `
-<div class="acrylic">
-  <p>Copyright(c) CodeZhangBorui & PowerCode 2022, All Right Reserved.</p>
-  <p>由 Github Page 提供支持</p>
-  <p>联系站长：[Discord] Code张博睿#4080</p>
-  <p>
-    主页:
-    <a href="https://codezhangborui.github.io/" target="_blank" style="color: #010101;">
-      https://codezhangborui.github.io/
-    </a>
-  </p>
-  <p>
-    Github 仓库:
-    <a href="https://github.com/CodeZhangBorui/codezhangborui.github.io/" target="_blank" style="color: #010101;">
-      https://github.com/CodeZhangBorui/codezhangborui.github.io/
-    </a>
-  </p>
-  <iframe
-    src="https://ghbtns.com/github-btn.html?user=codezhangborui&repo=codezhangborui.github.io&type=star&count=true"
-    frameborder="0" scrolling="0" width="150" height="20" title="GitHub Star"></iframe>
+<div class="footblock">
+  <div class="acrylic">
+    <p>Copyright(c) CodeZhangBorui & PowerCode 2022, All Right Reserved.</p>
+    <p>由 Github Page 提供支持</p>
+    <p>联系站长：[Discord] Code张博睿#4080</p>
+    <p>
+      主页:
+      <a href="https://codezhangborui.github.io/" target="_blank" style="color: #010101;">
+        https://codezhangborui.github.io/
+      </a>
+    </p>
+    <p>最后更新：2022-6-29</p>
+  </div>
 </div>
 `
   document.getElementById("footblock").innerHTML = fillContent;
