18b83389e9edc28ea4eefd0486296eff2682d108
CodeZhangBorui <zhangborui.2008@foxmail.com>
2022-07-15 14:29:30 +0800

    fix appPortal wrong gateway bug

--- apps/PowerCode_AppsLib_874fwe5df/js/appPortal.js
+++ apps/PowerCode_AppsLib_874fwe5df/js/appPortal.js
@@ -1,12 +1,14 @@
-vls = verifyLoginStatus();
-if(
-    vls == "NOT_LOGIN" || 
-    vls == "USER_NOT_FOUND" || 
-    vls == "SHA512PWD_NOT_FOUND" ||
-    vls == "WRONG_AUTHCODE" ||
-    vls == "WRONG_PWD"
-) {
-    setTimeout("window.location='/loginx'",2000)
-} else {
-    setTimeout("window.location='/apps/PowerCode_PowerStore_874fwe5df/'",5000)
+function appPortal(appID) {
+    vls = verifyLoginStatus();
+    if (
+        vls == "NOT_LOGIN" ||
+        vls == "USER_NOT_FOUND" ||
+        vls == "SHA512PWD_NOT_FOUND" ||
+        vls == "WRONG_AUTHCODE" ||
+        vls == "WRONG_PWD"
+    ) {
+        setTimeout("window.location='/loginx'", 2000)
+    } else {
+        setTimeout(`window.location='/apps/${appID}/'`, 5000)
+    }
 }
